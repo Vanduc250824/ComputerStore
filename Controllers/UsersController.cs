929904ca6f31784919ae808d8a407ed11c028547
Vanduc250824 <leduc250824@gmail.com>
2025-03-13 03:37:53 +0700

    update

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Computer_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Computer_Store.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
